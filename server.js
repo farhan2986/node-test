666c819f08438071425bbc222238e8f71e173157
Farhan Shahid <farhan2986@gmail.com>
2018-01-26 12:54:58 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var express = require('express'),
   app = express(),
-  bodyParser = require('body-parser'),
   routes = require('./api/routes/routes')
   port = process.env.PORT || 3000;
 
@@ -10,4 +9,4 @@ app.set('views', __dirname + '/api/views');
 app.set('view engine', 'pug');
 app.listen(port);
 
-console.log('CareerAxiom Node test assignment: ' + port);
+console.log('Node test running: ' + port);
